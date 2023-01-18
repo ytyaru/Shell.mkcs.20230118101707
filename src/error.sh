c1d3979201c9e4a3c4cb8b0ba7569723acf14845
ytyaru <yttry0@gmail.com>
2023-01-18 16:45:18 +0900

    パラメータ解析修正。-dオプション追加。ヘルプ修正。ハイライト表示リファクタリング。

--- src/error.sh
+++ src/error.sh
@@ -1,20 +1,29 @@
 #!/usr/bin/env bash
-Error() {
+Error() { L=$BASH_LINENO; Highlight $L BR 'ERROR' "$@"; exit $L; }
+Success() { Highlight $BASH_LINENO BG '' "$@"; }
+Debug() { Highlight $BASH_LINENO BB 'DEBUG' "$@"; }
+Warn() { Highlight $BASH_LINENO BY 'WARN' "$@"; }
+Highlight() { #$1:$BASH_LINENO, $2:Color(R,G,B,Y,C,M,K,W,B*...), $3:Header, $4...:Message
+	local L="$1"
+	local C=91
+	local H="$3"
+	case $2 in
+		"K") C=30;;	"BK") C=90;;
+		"R") C=31;;	"BR") C=91;;
+		"G") C=32;;	"BG") C=92;;
+		"Y") C=33;;	"BY") C=93;;
+		"B") C=34;;	"BB") C=94;;
+		"M") C=35;;	"BM") C=95;;
+		"C") C=36;;	"BC") C=96;;
+		"W") C=37;;	"BW") C=97;;
+	esac
 	Reset() { echo -en "\e[0m"; }
 	Bold() { echo -en "\e[1m"; }
-	LightRed() { echo -en "\e[91m"; }
-	Header() { Bold; LightRed; echo -en 'ERROR '"$ERR_CODE"': '; Reset; LightRed; }
-	Show() { Reset; echo -e "$(Header)""$msg" 1>&2; Reset; }
-	local ERR_CODE=$BASH_LINENO; [ 0 -eq $ERR_CODE ] && ERR_CODE=255;
-	local args=("$@"); local msg="$(IFS=$'\n'; echo -e "${args[*]}")"
-	Show
-	exit $ERR_CODE
-}
-Success() {
-	local args=("$@"); local msg="$(IFS=$'\n'; echo -e "${args[*]}")"
-	Reset() { echo -en "\e[0m"; }
-	Bold() { echo -en "\e[1m"; }
-	LightGreen() { echo -en "\e[92m"; }
-	Show() { Reset; Bold; LightGreen; echo -e "$msg" 1>&2; Reset; }
+	Color() { echo -en "\e[${C}m"; }
+	Header() { [ -n "$H" ] && { Bold; Color; echo -en "$H $L"': '; Reset; } || :; }
+	Show() { Reset; Header; Color; echo -e "$msg" 1>&2; Reset; }
+	#local ERR_CODE=$BASH_LINENO; [ 0 -eq $ERR_CODE ] && ERR_CODE=255;
+	#local args=("$@"); local msg="$(IFS=$'\n'; echo -e "${args[*]}")"
+	local args=("${@:4}"); local msg="$(IFS=$'\n'; echo -e "${args[*]}")"
 	Show
 }
--- src/mkcs.sh
+++ src/mkcs.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -Ceu
+#set -Ceu
 #---------------------------------------------------------------------------
 # C#のクラスライブラリとNUnitのプロジェクトをもったソリューション一式を作成する
 # CreatedAt: 2023-01-18
@@ -11,6 +11,7 @@ Run() {
 	local PJ=;
 	local CLS=;
 	local IS_RUN_TEST=;
+	local DEBUG=;
 	ParseCommand() {
 		THIS_NAME=`basename "$BASH_SOURCE"`
 		SUMMARY='C#のクラスライブラリとNUnitのプロジェクトをもったソリューション一式を作成する。'
@@ -18,15 +19,12 @@ Run() {
 		ARG_FLAG=; ARG_OPT=;
 		Help() { eval "echo -e \"$(cat help.txt)\""; }
 		Version() { echo "$VERSION"; }
-		while getopts ":hvt:" OPT; do
+		while getopts ":hvtd" OPT; do
 		case $OPT in
 			h) Help; exit 0;;
 			v) Version; exit 0;;
-			t) IS_RUN_TEST=1; shift; ;;
-			*) {	[ '?' == "$OPT" ] && break || :;
-					[ -z "$PJ" ] && { PJ="$OPT"; continue; } || :;
-					[ -z "$CLS" ] && { CLS="$OPT"; continue; } || :;
-				} ;;
+			t) IS_RUN_TEST=1; ;;
+			d) DEBUG=1; ;;
 		esac
 		done
 		shift $(($OPTIND - 1))
@@ -35,59 +33,53 @@ Run() {
 			case $1 in
 			-h|--help) Help; exit 0;;
 			-v|--version) Version; exit 0;;
-			-t|--test) IS_RUN_TEST=1; shift; ;;
-			*) echo "$OPTIND:$OPT aaaaaa"; ;;
+			-t|--test) IS_RUN_TEST=1; ;;
+			-d|--debug) DEBUG=1; ;;
 			esac
 		}
 		ParseSubCommand "$@"
 	}
 	SetArgs() {
+		for ARG; do
+			[[ $ARG =~ \-.+ ]] && { Debug "オプション引数なので飛ばす:$ARG"; continue; } || :;
+			[ -z "$PJ" ] && { Debug "PJにセットする:$ARG"; PJ="$ARG"; continue; } || :;
+			[ -z "$CLS" ] && { Debug "CLSにセットする:$ARG"; CLS="$ARG"; continue; } || :;
+		done
 		local D_PJ="$(date +CsPj%Y%m%d%H%M%S)"
 		local D_CLS='Human'
 		[ -z "$PJ" ] && PJ="$D_PJ" || :;
 		[ -z "$CLS" ] && CLS="$D_CLS" || :;
-		echo "PJ:$PJ"
-		echo "CLS:$CLS"
-		echo "IS_RUN_TEST:$IS_RUN_TEST"
-		[ -d "./$PJ" ] && Error "作成を中断しました。指定したプロジェクト名のディレクトリは既存です。別名か同名ディレクトリがない場所で再実行してください。: $PJ" || :;
+		Debug "PJ:$PJ"
+		Debug "CLS:$CLS"
+		Debug "IS_RUN_TEST:$IS_RUN_TEST"
+		Debug "DEBUG:$DEBUG"
+		[ -d "./$PJ" ] && Error "作成を中断しました。指定したプロジェクト名のディレクトリは既存です。別名に変えるか同名ディレクトリがない場所で再実行してください。: $PJ" || :;
 	}
 	MakeProjects() {
 		dotnet new sln -o "$PJ"
 		cd "$PJ"
 		dotnet new classlib -o Source -n "$PJ"
 		dotnet new nunit -o Test
-		#dotnet new classlib -o "$PJ"
-		#dotnet new nunit -o "${PJ}.Test"
-		#mv "$PJ" "Source"
-		#mv "${PJ}.Test" "Test"
-		#mv "./Test/${PJ}.Test.csproj" "./Test/${PJ}.csproj"
-		#dotnet sln add "./Test/${PJ}.csproj"
 		dotnet sln add "./Test/Test.csproj"
 		cd Test
 		dotnet add reference "../Source/${PJ}.csproj"
-		#dotnet add reference "../Source/Source.csproj"
 		cd ..
 	}
 	MakeSourceCodeTpl() {
 		rm ./Source/Class1.cs
 		rm ./Test/UnitTest1.cs
-		#cp "$HERE/tpl/Human.cs" "./Source/${CLS}.cs"
-		#cp "$HERE/tpl/Test.cs" "./Test/${CLS}.cs"
-		#WriteTplFile() { echo -e "$(eval "echo -e \"$(cat "$1")\"")" >| "$2"; }
 		export PJ="$PJ"
 		export CLS="$CLS"
 		WriteTplFile() { cat "$1" | mo >| "$2"; }
 		WriteTplFile "$HERE/tpl/Human.cs" "./Source/${CLS}.cs"
 		WriteTplFile "$HERE/tpl/Test.cs" "./Test/${CLS}.cs"
 	}
-	RunTest() {
-		[ $IS_RUN_TEST -eq 1 ] && { cd "$PJ"; dotnet test; cd..; } || :;
-	}
+	RunTest() { [[ $IS_RUN_TEST -eq 1 ]] && dotnet test || :; }
 	ParseCommand "$@"
 	SetArgs "$@"
 	MakeProjects
 	MakeSourceCodeTpl
 	Success "C#プロジェクトの作成が完了しました：$PJ"
-	#RunTest
+	RunTest
 }
 Run "$@"
