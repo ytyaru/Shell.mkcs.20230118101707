c1574ef4e024adf2a164f5419718c9aa987ad0d7
ytyaru <yttry0@gmail.com>
2023-01-18 17:55:00 +0900

    カレントディレクトリに出力されないバグを修正した。

--- src/mkcs.sh
+++ src/mkcs.sh
@@ -6,8 +6,7 @@
 #---------------------------------------------------------------------------
 Run() {
 	THIS="$(realpath "${BASH_SOURCE:-0}")"; HERE="$(dirname "$THIS")"; PARENT="$(dirname "$HERE")"; THIS_NAME="$(basename "$THIS")"; APP_ROOT="$PARENT";
-	cd "$HERE"
-	[ -f 'error.sh' ] && . error.sh
+	[ -f "$HERE/error.sh" ] && . "$HERE/error.sh"
 	local PJ=;
 	local CLS=;
 	local IS_RUN_TEST=;
